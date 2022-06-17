7b4eac20f4eae341d214ea428324ebc137672513
Igor Santos <igor.ox13@gmail.com>
2022-06-17 20:36:33 -0300

    mudança de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        escuro:{
+          50: "#6f3824",
+          100: "#592d1d",
+          200: "#472417"
+        },
+        claro: {
+          50: "#fbf7e4",
+          100: "#f4e8b4"
+        },
+        medio: {
+          50: "#f5ac94",
+          100: "#ee754c",
+          200: "#e54a16"
+        }
+      }
+    },
   },
   plugins: [],
 }
